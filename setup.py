2428f5e04cc77d6002096beb4e4526f65c5ea4fa
RHA1705 <garbarzroman47@gmail.com>
2023-12-28 05:16:51 +0100

    new

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='clean_folder',
     version='1',
-    description='Cleans your foder',
-    url='http://github.com/dummy_user/useful',
-    author='Flying Circus',
-    author_email='flyingcircus@example.com',
+    description='Sort files in your folder',
+    url='https://github.com/RHA1705/HW7.git',
+    author='Roman Harbazh',
+    author_email='garbarzroman47@gmail.com',
     packages=find_namespace_packages(),
     entry_points={'console_scripts': ['clean-folder = clean_folder.clean:main']}
 )
